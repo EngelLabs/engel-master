05cc4648f1a6e78bdeb53c3f68750a2d5004fcd9
Tinoy Thomas <tinoytpm1@gmail.com>
2022-02-06 18:08:43 -0800

    [core] Switch logger to remote repository

--- src/core/utils/logger.js
+++ src/core/utils/logger.js
@@ -1,27 +1,3 @@
-const winston = require('winston');
 const baseConfig = require('./baseConfig');
 
-
-const options = {
-        format: winston.format.combine(
-                winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:SSS' }),
-                winston.format.colorize({ level: true }),
-                winston.format.printf(info => `[${info.timestamp}] [${info.level}] ${info.message}`),
-        ),
-        transports: [
-                new winston.transports.Console(),
-        ],
-};
-
-const logger = winston.createLogger(options);
-
-logger.on('error', err => {
-        try { logger.error('[Logger] Something went wrong.') } catch { }
-        console.error(err);
-});
-
-logger.info(`[Logger] Level "${baseConfig.logger.level}".`);
-
-logger.level = baseConfig.logger.level;
-
-module.exports = logger;
+module.exports = require('@timbot/logger')(baseConfig);
