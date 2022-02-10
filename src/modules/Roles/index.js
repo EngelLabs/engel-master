f6c03239be9343f7c704047479e9824c8ff4a4fc
Tinoy Thomas <tinoytpm1@gmail.com>
2022-02-10 11:07:26 -0800

    [modules.roles] New module

---
+++ src/modules/Roles/index.js
@@ -0,0 +1,13 @@
+const Module = require('../../core/structures/Module');
+
+
+class Roles extends Module {
+        constructor() {
+                super();
+
+                this.info = 'Commands to manage server roles';
+        }
+}
+
+
+module.exports = Roles;
