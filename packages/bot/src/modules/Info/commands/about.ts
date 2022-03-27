726989e1bf7c4a438d38df378ae7ec9ded3357b4
Tinoy <tinoytpm1@gmail.com>
2022-03-27 01:12:14 -0700

    feat(bot): add more info to `about` command

--- packages/bot/src/modules/Info/commands/about.ts
+++ packages/bot/src/modules/Info/commands/about.ts
@@ -1,6 +1,15 @@
+import * as os from 'os';
 import * as prettyMS from 'pretty-ms';
 import Command from '../../../core/structures/Command';
 import type Info from '..';
+
+const reduce = (err: Error | null, data: { [key: string]: string }) => {
+        if (err) {
+                throw err;
+        }
+
+        return Object.values(data).reduce((prev, curr) => Number(curr) + prev, 0);
+};
 
 export default new Command<Info>({
         name: 'about',
@@ -8,23 +17,34 @@ export default new Command<Info>({
         dmEnabled: true,
         info: 'Get information about the bot',
         cooldown: 20000,
-        execute: function (ctx) {
+        execute: async function (ctx) {
                 const owner = ctx.eris.users.get(ctx.config.author.id);
 
                 if (!owner) return ctx.addErrorReaction();
 
+                const [events, guildCounts, memberCounts, userCounts] = await ctx.redis.multi()
+                        .hgetall('engel:events')
+                        .hgetall('engel:guilds')
+                        .hgetall('engel:members')
+                        .hgetall('engel:users')
+                        .exec();
+
+                const loadAvg = os.loadavg().join(', ');
+
                 const embed = {
-                        description: `[Support server](${ctx.config.guilds.official.invite} "Very cool server, join it")`,
+                        description: `[Support server](${ctx.config.guilds.official.invite} "Very cool server, join it for 10/10 experience ~ timtoy")`,
                         fields: [
                                 { name: 'Owner', value: owner.mention, inline: true },
-                                { name: 'Guilds', value: ctx.eris.guilds.size.toString(), inline: true },
-                                { name: 'Users', value: ctx.eris.users.size.toString(), inline: true },
-                                { name: 'Events', value: '...', inline: true },
+                                { name: 'Guilds', value: reduce(...guildCounts).toString(), inline: true },
+                                { name: 'Members', value: reduce(...memberCounts).toString(), inline: true },
+                                { name: 'Users', value: reduce(...userCounts).toString(), inline: true },
+                                { name: 'WS Recv', value: `${events[1].ws / 20}/sec`, inline: true },
+                                { name: 'HTTP', value: `${events[1].http / 20}/sec`, inline: true },
                                 { name: 'Uptime', value: prettyMS(Math.floor(process.uptime()) * 1000), inline: true },
-                                { name: 'Process', value: `PID: ${process.pid}`, inline: true } // TODO: add more info here
+                                { name: 'Load (1m, 5m, 15m)', value: loadAvg, inline: true }
                         ],
                         author: {
-                                name: `${ctx.baseConfig.name}[${ctx.baseConfig.client.name}] v${ctx.baseConfig.version}`,
+                                name: `${ctx.baseConfig.client.name}[${ctx.baseConfig.client.state}, v${ctx.baseConfig.version}]`,
                                 url: ctx.eris.user.avatarURL
                         },
                         timestamp: new Date().toISOString(),
