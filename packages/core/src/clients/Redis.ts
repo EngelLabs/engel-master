4a7cafeef9699e0877b925187579163c512f12f5
Tinoy <tinoytpm1@gmail.com>
2022-03-27 01:07:29 -0700

    feat(core): make logs for `Redis` client optional

--- packages/core/src/clients/Redis.ts
+++ packages/core/src/clients/Redis.ts
@@ -4,26 +4,27 @@ import type Core from '../structures/Core';
 
 const IORedis = ioredis;
 
-export default function Redis(core: Core): ioredis.Redis {
-        const log = (message?: any, level?: types.LogLevels, prefix: string = 'Redis') => {
-                core.log(message, level, prefix);
-        };
-
+export default function Redis(core: Core, shouldLog: boolean = true): ioredis.Redis {
         const client = new IORedis(
                 core.baseConfig.redis.port,
                 core.baseConfig.redis.host
         );
 
-        client
-                .on('ready', () => {
-                        log('Connected.', 'info');
-                })
-                .on('close', () => {
-                        log('Disconnected.', 'info');
-                })
-                .on('error', (err: any) => {
-                        log(err, 'error');
-                });
+        if (shouldLog) {
+                const log = (message?: any, level?: types.LogLevels, prefix: string = 'Redis') => {
+                        core.log(message, level, prefix);
+                };
+                client
+                        .on('ready', () => {
+                                log('Connected.', 'info');
+                        })
+                        .on('close', () => {
+                                log('Disconnected.', 'info');
+                        })
+                        .on('error', (err: any) => {
+                                log(err, 'error');
+                        });
+        }
 
         return client;
 }
