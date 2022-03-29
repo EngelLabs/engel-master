a0d899c88c312db3c5e3301bd26b15b060d73f4f
Tinoy Thomas <tinoytpm1@gmail.com>
2022-03-29 09:06:51 -0700

    refactor(bot): update `about` command

--- packages/bot/src/modules/Info/commands/about.ts
+++ packages/bot/src/modules/Info/commands/about.ts
@@ -9,14 +9,6 @@ const roundIfWhole = (num: number) => {
         return num >= 1 ? Math.round(num) : num;
 };
 
-const reduce = (err: Error | null, data: { [key: string]: string }) => {
-        if (err) {
-                throw err;
-        }
-
-        return Object.values(data).reduce((prev, curr) => Number(curr) + prev, 0);
-};
-
 export default new Command<Info>({
         name: 'about',
         alwaysEnabled: true,
@@ -29,34 +21,42 @@ export default new Command<Info>({
 
                 if (!owner) return ctx.addErrorReaction();
 
-                const [events, guildCounts, memberCounts, userCounts] = await ctx.redis.multi()
-                        .hgetall('engel:events')
-                        .hgetall('engel:guilds')
-                        .hgetall('engel:members')
-                        .hgetall('engel:users')
-                        .exec();
+                const allClusterStats = await ctx.redis.hgetall('engel:clusters');
 
-                const loadAvg = os.loadavg().join(', ');
+                let guildCount = 0, memberCount = 0, userCount = 0, wsEvents = 0, httpEvents = 0;
+
+                for (const rawClusterStats of Object.values(allClusterStats)) {
+                        const clusterStats = JSON.parse(rawClusterStats);
+
+                        guildCount += clusterStats.guilds;
+                        memberCount += clusterStats.members;
+                        userCount += clusterStats.userCount;
+                        wsEvents += clusterStats.ws;
+                        httpEvents += clusterStats.http;
+                }
+
+                const loadAvg = os.loadavg().map(i => i.toFixed(2)).join(', ');
 
                 const embed = {
                         description: `[Support server](${ctx.config.guilds.official.invite} "Very cool server, join it for 10/10 experience ~ timtoy")`,
                         fields: [
                                 { name: 'Owner', value: owner.mention, inline: true },
-                                { name: 'Guilds', value: reduce(...guildCounts).toString(), inline: true },
-                                { name: 'Members', value: reduce(...memberCounts).toString(), inline: true },
-                                { name: 'Users', value: reduce(...userCounts).toString(), inline: true },
-                                { name: 'WS Recv', value: `${roundIfWhole(events[1].ws / 20)}/sec`, inline: true },
-                                { name: 'HTTP', value: `${roundIfWhole(events[1].http / 20)}/sec`, inline: true },
+                                { name: 'Guilds', value: guildCount.toString(), inline: true },
+                                { name: 'Members', value: memberCount.toString(), inline: true },
+                                { name: 'Users', value: userCount.toString(), inline: true },
                                 { name: 'Uptime', value: prettyMS(Math.floor(process.uptime()) * 1000), inline: true },
-                                { name: 'Load (1m, 5m, 15m)', value: loadAvg, inline: true }
+                                { name: 'Load (1m, 5m, 15m)', value: loadAvg, inline: true },
+                                { name: 'WS Recv', value: `${roundIfWhole(wsEvents / 10)}/sec`, inline: true },
+                                { name: 'HTTP', value: `${roundIfWhole(httpEvents / 10)}/sec`, inline: true }
                         ],
                         author: {
                                 name: `${ctx.baseConfig.client.name}[${ctx.baseConfig.client.state}, v${ctx.baseConfig.version}]`,
-                                url: ctx.eris.user.avatarURL
+                                url: 'https://bit.ly/36QzBAF',
+                                icon_url: ctx.eris.user.avatarURL
                         },
                         timestamp: new Date().toISOString(),
                         footer: {
-                                text: `Built with ${ctx.baseConfig.lib}`
+                                text: `Cluster ${ctx.baseConfig.cluster.id} | Shard ${ctx.guild?.shard?.id ?? 0}`
                         }
                 };
 
