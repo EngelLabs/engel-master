4d6ad7f7f824682de5656473a4efe8925dbe8269
Tinoy Thomas <tinoytpm1@gmail.com>
2022-07-25 22:10:42 -0700

    fix(bot): remove "t." dm prefix

--- packages/bot/scripts/config.js
+++ packages/bot/scripts/config.js
@@ -161,7 +161,6 @@ const defaultConfig = {
                         '?'
                 ],
                 dm: [
-                        't.',
                         '?',
                         '!',
                         ''
