e4d60e7f8d05a524651fb396a1b4826e159a6dfe
Tinoy Thomas <tinoytpm1@gmail.com>
2022-07-25 22:34:02 -0700

    fix(env-util): some typos and general improvements

--- packages/env-util/src/index.js
+++ packages/env-util/src/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 /* eslint-disable-next-line new-parens */
-const NULL = new (class NULL { [Symbol] });
+const NULL = new function NULL() { };
 
 const converters = {
         str: value => {
@@ -44,100 +44,98 @@ const converters = {
         }
 };
 
-const createConvertionMethod = (type, constructor) => {
+const createConversionMethod = (type, constructor) => {
         return function (key, fallback = NULL, ...extraArgs) {
                 return this._getEnv(key, fallback, converters[type], constructor, ...extraArgs);
         };
 };
 
-class Env {
-        _config = {
+function Env(options) {
+        if (!(this instanceof Env)) {
+                return new Env(options);
+        }
+
+        this._config = {
                 ignoreFallbacks: false,
                 ignoreMissing: false,
                 path: path.join(process.cwd(), '../../.env')
         };
+        this.config(options);
+}
 
-        constructor(options) {
-                this.config(options);
+Env.prototype.Env = Env.Env = Env;
+
+Env.prototype._getEnv = function (key, fallback, converter, constructor, ...extraArgs) {
+        if (this._config.ignoreFallbacks) {
+                fallback = NULL;
         }
 
-        _getEnv(key, fallback, converter, constructor, ...extraArgs) {
-                if (this._config.ignoreFallbacks) {
-                        fallback = NULL;
-                }
-
-                if (!Object.prototype.hasOwnProperty.call(process.env, key)) {
-                        if (fallback === NULL) {
-                                if (this._config.ignoreMissing) {
-                                        return;
-                                }
-
-                                throw new Error(`process.env.${key} is undefined.`);
+        if (!Object.prototype.hasOwnProperty.call(process.env, key)) {
+                if (fallback === NULL) {
+                        if (this._config.ignoreMissing) {
+                                return;
                         }
 
+                        throw new Error(`process.env.${key} is undefined.`);
+                }
+
+                return fallback;
+        }
+
+        const ret = converter(process.env[key], ...extraArgs);
+
+        if (ret === NULL || ret instanceof Error) {
+                if (this._config.ignoreErrors) {
                         return fallback;
                 }
 
-                const ret = converter(process.env[key], ...extraArgs);
+                throw new Error(
+                        `process.env.${key}: "${process.env[key]}" could not be converted to "${constructor.name}".`
+                );
+        }
 
-                if (ret === NULL || ret instanceof Error) {
-                        if (this._config.ignoreErrors) {
-                                return fallback;
-                        }
+        return ret;
+};
 
+Env.prototype.load = function load() {
+        const { error } = require('dotenv').config(this._config);
+
+        if (error) {
+                throw error;
+        }
+
+        return this;
+};
+
+Env.prototype.config = function config(options) {
+        this._config = Object.assign(this._config, options);
+
+        return this;
+};
+
+/* Conversion methods */
+Env.prototype.string = Env.prototype.str = createConversionMethod('str', String);
+Env.prototype.integer = Env.prototype.int = createConversionMethod('int', Number);
+Env.prototype.boolean = Env.prototype.bool = createConversionMethod('bool', Boolean);
+Env.prototype.array = Env.prototype.arr = function (key, type = 'str', fallback = NULL) {
+        const converter = (value, type = 'str') => {
+                const arr = value.split(',');
+
+                const ret = arr.map(converters[type]);
+
+                const idx = ret.findIndex(o => o === NULL);
+
+                if (idx !== -1) {
                         throw new Error(
-                                `process.env.${key}: "${process.env[key]}" could not be converted to "${constructor.name}".`
+                                `process.env.${key}.${idx}: Array element "${arr[idx]}" could not be converted to "${type}"`
                         );
                 }
 
                 return ret;
-        }
+        };
 
-        load() {
-                const { error } = require('dotenv').config(this._config);
-
-                if (error) {
-                        throw error;
-                }
-
-                return this;
-        }
-
-        config(options) {
-                this._config = Object.assign(this._config, options);
-
-                return this;
-        }
-
-        /* Conversion methods */
-        str = createConvertionMethod('str', String);
-        int = createConvertionMethod('int', Number);
-        bool = createConvertionMethod('bool', Boolean);
-        arr(key, type = 'str', fallback = NULL) {
-                const converter = (value, type = 'str') => {
-                        const arr = value.split(',');
-
-                        const ret = arr.map(converters[type]);
-
-                        const idx = ret.findIndex(o => o === NULL);
-
-                        if (idx !== -1) {
-                                throw new Error(
-                                        `process.env.${key}.${idx}: Array element "${arr[idx]}" could not be converted to "${type}"`
-                                );
-                        }
-
-                        return ret;
-                };
-
-                return this._getEnv(key, fallback, converter, Array, type);
-        }
-
-        /* Aliases */
-        string = this.str;
-        integer = this.int;
-        boolean = this.bool;
-}
+        return this._getEnv(key, fallback, converter, Array, type);
+};
 
 const env = new Env();
 
@@ -146,5 +144,3 @@ try {
 } catch { }
 
 module.exports = env;
-
-module.exports.Env = Env;
